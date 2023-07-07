87a2bb4885acbedf3cc2e53f40652463296605cf
Athena Stamos <athenas1806@gmail.com>
2023-07-07 17:25:27 +0000

    new branch

--- R_linear_model.r
+++ R_linear_model.r
@@ -1,13 +1,19 @@
+# Import needed libraries
 library(ggplot2)
+
+# Set notebook variables
 
 filename = "regrex1.csv"
 
+# Read regrex1.csv file
 dataset = read.csv(filename)
 dataset
 
+# Fitting Linear Regression to the Dataset
 model = lm(formula = y ~ x,
                data = dataset)
 
+# Scatter plot of original dataset
 png("r_orig.png")
 ggplot() +
   geom_point(aes(x = dataset$x, y = dataset$y),
@@ -16,11 +22,14 @@ ggplot() +
   ylab('y')
 dev.off()
 
+# Linear model of data set
 ggplot() + geom_line(aes(x = dataset$x, y = predict(model, newdata = dataset)),
             colour = 'blue') +
   ggtitle('Linear Model of y vs. x') +
   xlab('x') +
   ylab('y')
+
+# Visualize the Linear Regression
 
 png("r_lm.png")
 ggplot() +
